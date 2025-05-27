c7e931126a17137f72c41ced2252340e6c255379
scopalaffairs <ministry@scopal-affairs.com>
2025-05-27 14:28:49 +0200

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -206,7 +206,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("Loading submission payload:", payload);
 
-    fetch("http://127.0.0.1:5050/astro/api/compute_rising_lines", {
+    fetch("/astro/api/compute_rising_lines", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
